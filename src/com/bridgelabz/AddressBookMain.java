f07dd192b24d8dcd6a197af33f851293813ec4d2
Kirandhanwai <kirandhanwai123@gmail.com>
2022-10-21 21:23:39 +0530

    Added : Adding New Contacts

--- src/com/bridgelabz/AddressBookMain.java
+++ src/com/bridgelabz/AddressBookMain.java
@@ -4,9 +4,7 @@ public class AddressBookMain {
 	
 	public static void main(String[] args) {
 		System.out.println("Welcome to addressBook java Program");
-		
-		Contact contact = new Contact("Kiran", "Dhanwai", "Aurangabad", "Aurangabad", 123456, "Maharashtra",
-				"1234567890", "kiran@gmail.com");
-		System.out.println( contact );
-		}
+		 AddressBook addressBook =new AddressBook();
+		 addressBook.addNewContact();
+	}
 }
--- src/com/bridgelabz/Contact.java
+++ src/com/bridgelabz/Contact.java
@@ -5,7 +5,7 @@ public class Contact {
 	private String lastName;
 	private String address;
 	private String city;
-	private int zip;
+	private String zip;
 	private String state;
 	private String phoneNo;
 	private String email;
@@ -16,25 +16,20 @@ public class Contact {
 		// TODO Auto-generated constructor stub
 	}
 
-	public Contact(String firstName, String lastName, String address, String city, int zip, String state, String phoneNo,
-		String email) {
+	public Contact(String firstName, String lastName, String address, String city, String zip, String state, String phoneNo,String email) {
 	this.firstName = firstName;
 	this.lastName = lastName;
 	this.address = address;
 	this.city = city;
 	this.zip = zip;
-	this.state = state;
+	this.state = state; 
 	this.phoneNo = phoneNo;
 	this.email = email;
-}
+   }
 
 	@Override
 	public String toString() {
 		return "Contact [firstName=" + firstName + ", lastName=" + lastName + ", address=" + address + ", city=" + city
 				+ ", zip=" + zip + ", state=" + state + ", phoneNo=" + phoneNo + ", email=" + email + "]";
-	}
-
-
-	
-	
+	}	
 }
